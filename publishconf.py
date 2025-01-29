e1b8e8a1cd1549e59bd976415590bb3acbbdfc83
michael.g.russell <michael.g.russell@baesystems.us>
2025-01-29 15:56:41 -0700

    Trying with pelicanconf

--- publishconf.py
+++ publishconf.py
@@ -1,24 +1,81 @@
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+# pylint: disable=missing-function-docstring,missing-module-docstring
 
-# pylint: disable=wildcard-import,missing-module-docstring,wrong-import-position,unused-wildcard-import
+from math import ceil
+from string import capwords
 
-import os
-import sys
+from pelican import signals
 
-sys.path.append(os.curdir)
-from pelicanconf import *
-
-# If your site is available via HTTPS, make sure SITEURL begins with https://
+AUTHOR = "Michael Russell"
+SITENAME = "Runic Reflections"
 SITEURL = "https://michaelrussell4.github.io/runic-reflections"
-RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+PATH = "content"
+ARTICLE_PATHS = ["poetry"]
+ARTICLE_SAVE_AS = "{date:%Y}/{slug}.html"
+ARTICLE_URL = "{date:%Y}/{slug}.html"
+ARTICLE_ORDER_BY = "reversed-date"
 
-DELETE_OUTPUT_DIRECTORY = True
+TIMEZONE = "America/Denver"
 
-# Following items are often useful when publishing
+DEFAULT_LANG = "en"
 
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = ""
+# Feed generation is usually not desired when developing
+SOCIAL = None
+FEED_ALL_ATOM = None
+FEED_ALL_RSS = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 6
+USE_FOLDER_AS_CATEGORY = True
+
+# Theme
+THEME = "rr-theme"
+THEME_STATIC_DIR = f"{THEME}/static"
+TAILWIND_CSS = "tailwind.min.css"
+
+
+def title_case_custom(title):
+    # Use regex to handle apostrophes correctly
+    # This regex will ensure that letters following apostrophes are not capitalized
+    title = capwords(title)
+
+    return title
+
+
+def group_articles(articles):
+    """Group articles by the first letter of their title and sort them alphabetically."""
+    grouped = {}
+
+    # Group articles by the first letter
+    for article in articles:
+        first_letter = article.title[0].upper()
+        if first_letter not in grouped:
+            grouped[first_letter] = []
+        grouped[first_letter].append(article)
+
+    # Sort the articles in each group alphabetically by title
+    for letter in grouped:
+        grouped[letter].sort(key=lambda x: x.title)
+
+    # Create a sorted dictionary based on the keys (letters)
+    sorted_grouped = {letter: grouped[letter] for letter in sorted(grouped.keys())}
+
+    return sorted_grouped
+
+
+JINJA_FILTERS = {
+    "group_articles": group_articles,
+    "ceil": ceil,
+    "title_case_custom": title_case_custom,
+}
+
+
+def sort_articles(articles):
+    return sorted(articles, key=lambda article: article.title)
+
+
+def register():
+    signals.article_generator_finalized.connect(sort_articles)
